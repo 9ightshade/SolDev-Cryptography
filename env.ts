84d86b1e1b7731254b1d04a4c6527ea5cf674eff
9ightshade <aniekemeusanga@gmail.com>
2024-05-17 09:03:49 +0100

    sucessfully loaded secret key after alot, it works so please don't touch

--- env.ts
+++ env.ts
@@ -1,11 +1,8 @@
-import { config } from "dotenv";
-import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+require('dotenv').config();
 
+const Loaded_key_pair = process.env.SECRET_KEY;
 
-
-
-const Loaded_key_pair = getKeypairFromEnvironment("SECRET_KEY");
-console.log( Loaded_key_pair);
+console.log(Loaded_key_pair);
 console.log(
   `✅ Finished! We've loaded our secret key securely, using an env file!`
 );
